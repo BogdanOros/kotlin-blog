5cfd8257ddf7f3f8a8a4d743459fa581dd08ee8c
boros <bogdan.oros@n-cube.co.uk>
2018-01-15 23:48:28 +0200

    SecurityConfiguration refactored

--- src/main/kotlin/com/boros/configuration/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/boros/configuration/security/SecurityConfiguration.kt
@@ -1,23 +1,25 @@
 package com.boros.configuration.security
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
-class SecurityConfiguration : WebSecurityConfigurerAdapter() {
+class SecurityConfiguration {
 
-    // todo for public urls use BasicHttpAuth, for private JWT
-    override fun configure(http: HttpSecurity?) {
-        http!!.csrf().and().cors().disable()
-        http.requestMatchers()
-                .antMatchers("/api/**")
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/**")
-                .authenticated()
-                .and()
-                .httpBasic()
+    @Configuration
+    @Order(2)
+    class BasicSecurityConfiguration : WebSecurityConfigurerAdapter() {
+        override fun configure(http: HttpSecurity?) {
+            http!!.csrf().and().cors().disable()
+
+            http.authorizeRequests()
+                    .antMatchers("/api/**")
+                    .authenticated()
+                    .and()
+                    .httpBasic()
+        }
     }
 
 }
